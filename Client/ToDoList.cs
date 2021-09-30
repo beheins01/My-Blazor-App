77186b315a07b9c9a59b21656548de0239a0d2fa
beheins01 <62121607+beheins01@users.noreply.github.com>
2021-09-30 14:24:01 -0400

    Update ToDoList.cs

--- Client/ToDoList.cs
+++ Client/ToDoList.cs
@@ -1,5 +1,5 @@
-public class TodoItem
+public class TodoItem   // created a class called TodoItem
 {
-    public string Title { get; set; }
-    public bool IsDone { get; set; }
+    public string Title { get; set; }   // This is a getter and a setter for the title
+    public bool IsDone { get; set; }    // This is a getter and setter do determine if the task is done
 }
